ae713a2382eabfdd43a5923249fc69911d4562cc
Adam Good <dev.adamgood@gmail.com>
2025-05-07 11:54:03 -0400

    WIP reorganization

---
+++ app/controllers/user_controller.py
@@ -0,0 +1,20 @@
+
+from models.user import User
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def index():
+    pass
+
+def store():
+    pass
+
+def show():
+    pass
+
+def update():
+    pass
+
+def destroy():
+    pass
