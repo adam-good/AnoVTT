72d39e66ef9efe7fdf7e4ebd404b0f14d9e5ebcf
Adam Good <dev.adamgood@gmail.com>
2025-05-14 18:34:40 -0400

    Implemented statblock store call

--- webapp/controllers/statblock_controller.py
+++ webapp/controllers/statblock_controller.py
@@ -1,18 +1,23 @@
 from webapp.models.statblock import Statblock
 from webapp.database import session as db_session
-from flask import request, render_template
+from flask import request, render_template, url_for
 
 def index():
     result = Statblock.query.all()
     return ''.join([f'{statblock}' for statblock in result])
 
 def store():
-    name = request.form['name']
-    might = request.form['might']
-    edge = request.form['edge']
-    grit = request.form['grit']
-    wits = request.form['wits']
-    statblock = Statblock(name, might, edge, grit, wits)
-    db_session.add(statblock)
-    db_session.commit()
-    return render_template('statblock/create')
+    if request.method == "GET":
+        return render_template("statblock/create.html")
+    elif request.method == "POST":
+        name = request.form['name']
+        might = int(request.form['might'])
+        edge = int(request.form['edge'])
+        grit = int(request.form['grit'])
+        wits = int(request.form['wits'])
+        statblock = Statblock(name, might, edge, grit, wits)
+        db_session.add(statblock)
+        db_session.commit()
+        return render_template("statblock/create.html")
+    else:
+        return "fuck"
--- webapp/routes/statblock_bp.py
+++ webapp/routes/statblock_bp.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
-from webapp.controllers.statblock_controller import index
+from webapp.controllers.statblock_controller import index, store
 
 statblock_bp = Blueprint('statblock', __name__, url_prefix='/statblock')
 
 statblock_bp.route('/', methods=['GET'])(index)
+statblock_bp.route('/create', methods=['GET', 'POST'])(store)
