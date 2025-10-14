b83462096df01cc594016d8a2bd58ecf052c7a42
Adam Good <dev.adamgood@gmail.com>
2025-10-14 11:55:24 -0400

    slowly getting vite to run this mess

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,9 @@
 import { RouterProvider } from 'react-router';
 import './App.css';
 import Navbar from './components/Navbar.jsx';
-import Router from './router.jsx';
+// import Router from './router.jsx';
 import Home from './components/Home.jsx';
+
 const Header = () => {
   return (
     <div class='App-header'>
@@ -22,7 +23,7 @@ function App() {
     <div className="App">
     <Header />
     <div class='App-body'>
-      <Home />  //<RouterProvider router={Router} />
+      <h1>Hello from React</h1>
     </div>
     <Footer />
     </div>
