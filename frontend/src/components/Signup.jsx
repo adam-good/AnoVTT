b2ed60966a0b2e2c2254d53f0598091cb5ba7984
Adam Good <dev.adamgood@gmail.com>
2025-10-16 16:47:45 -0400

    added boilerplate functionality to signup

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,29 +1,57 @@
-
+import { useState } from "react";
 
 const SignupForm = () => {
+    const [formDate, setFormData] = useState({
+        username: '',
+        email: '',
+        password: ''
+    });
+
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value 
+        }));
+        console.log("State Updated");
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // TODO: Submit Form Data to Server
+        console.log(`Form Data Submitted: ${event.target}`);
+    };
+    
     return (
     <div className="form-container">
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="form-group">
-            <lable htmlFor="username">Username:</lable>
+            <label htmlFor="username">Username:</label>
             <input
                 type="text"
                 id="username"
                 name="username"
+                onChange={handleChange}
                 placeholder="Enter Username"
             />
-            <lable>Email:</lable>
+            </div>
+            <div className="form-group">
+            <label>Email:</label>
             <input
                 type="email"
                 id="email"
                 name="email"
+                onChange={handleChange}
                 placeholder="Enter Email"
             />
-            <lable>Password:</lable>
+            </div>
+            <div className="form-group">
+            <label>Password:</label>
             <input
                 type="password"
                 id="password"
                 name="password"
+                onChange={handleChange}
                 placeholder="password"
             />
             </div>
