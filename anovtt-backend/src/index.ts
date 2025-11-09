c0a0842ecf9d3cc6387f37866679a52ca08df95a
Adam Good <dev.adamgood@gmail.com>
2025-11-09 09:54:51 -0500

    Added rudementary user auth to backend

--- anovtt-backend/src/index.ts
+++ anovtt-backend/src/index.ts
@@ -1,23 +1,21 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import http from 'http';
-import { connectDB } from './db.js'
-import type { Int32, IntegerType } from 'mongodb';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import http from "http";
+import { connectDB } from "./db.js";
+import authRoutes from "./routes/auth.route.js";
 
-const HOSTNAME : string          = 'localhost';
-const PORT     : number          = 8080;
-const app      : express.Express = express();
+const HOSTNAME: string = "localhost";
+const PORT: number = 8080;
+const app: express.Express = express();
 dotenv.config();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-app.use((req: express.Request, res: express.Response) => {
-  console.log(req.headers);
-  res.send("hello world");
-});
+// Routes
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, (err: Error | undefined) => {
   console.log(`[*] Starting Server on ${PORT}`);
---
+++ anovtt-backend/src/routes/auth.route.ts
@@ -0,0 +1,77 @@
+import { response, Router, type Request, type Response } from "express";
+import { connectDB } from "../db.js";
+import { type User } from "../models/user.model.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import type { Collection, MongoClient } from "mongodb";
+
+const router: Router = Router();
+
+const BCRYPT_HASH_ROUNDS: number = 10;
+
+router.post(
+  "/register",
+  async (req: Request, res: Response): Promise<Response> => {
+    const { username, email, password } = req.body;
+    const hashedPassword: string = await bcrypt.hash(
+      password,
+      BCRYPT_HASH_ROUNDS,
+    );
+
+    try {
+      const client: MongoClient = await connectDB();
+      const collection: Collection<User> = client
+        .db()
+        .collection<User>("users");
+      const newUser: User = {
+        _id: "",
+        username,
+        email,
+        passwordHash: hashedPassword,
+      };
+
+      // Check if user exists
+      const existingUser: boolean =
+        (await collection.findOne({ email, username })) !== null;
+      if (existingUser) {
+        return res.status(400).json({ error: "User already exists" });
+      }
+
+      // Temp ID scheme until I make a better one
+      newUser._id = await bcrypt.hash(
+        newUser.username + new Date().toDateString,
+        0,
+      );
+      await collection.insertOne(newUser);
+      return res.status(201).json({ message: "User registered successfully" });
+    } catch (e) {
+      console.log(e);
+      return res.status(500).json({ error: "Server Error" });
+    }
+  },
+);
+
+// Login
+router.post("/login", async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  try {
+    const client = await connectDB();
+    const collection = client.db().collection<User>("users");
+    const user = await collection.findOne({ username });
+
+    if (!user || !(await bcrypt.compare(password, user.passwordHash))) {
+      return res.status(401).json({ error: "Invalid Credidentials" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, "secret", { expiresIn: "1h" });
+    res
+      .status(200)
+      .json({ token, user: { username: user.username, email: user.email } });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ error: "Server Error" });
+  }
+});
+
+export default router;
