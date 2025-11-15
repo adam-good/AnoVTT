bfd584a2f2cb9e592db4d0d8007dfa738e865b0c
Adam Good <dev.adamgood@gmail.com>
2025-11-15 14:10:29 -0500

    Revert to my way

--- anovtt-frontend/src/utils/result.ts
+++ anovtt-frontend/src/utils/result.ts
@@ -1,27 +1,9 @@
-// NOTE: Inspired by https://www.dennisokeeffe.com/blog/2024-07-14-creating-a-result-type-in-typescript
+export type Result<T, E> = { ok: T; error?: never } | { err: E; ok?: never };
 
-export type Ok<T> = {
-  _tag: "success";
-  data: T;
-};
-
-export type Err<E> = {
-  _tag: "failure";
-  error: E;
-};
-
-export type Result<T, E> = Ok<T> | Err<E>;
-
-export function ok<T>(value: T): Ok<T> {
-  return {
-    _tag: "success",
-    data: value,
-  };
+function ok<T>(value: T): Result<T, any> {
+  return { ok: value };
 }
 
-export function err<E>(error: E): Err<E> {
-  return {
-    _tag: "failure",
-    error: error,
-  };
+function err<E>(error: E): Result<any, E> {
+  return { err: error };
 }
