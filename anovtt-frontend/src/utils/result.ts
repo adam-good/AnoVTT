6095ce8da1a2cbfaa416fd9ea63ff2403f410832
Adam Good <dev.adamgood@gmail.com>
2025-11-15 13:57:26 -0500

    Trying out a result type

--- anovtt-frontend/src/utils/result.ts
+++ anovtt-frontend/src/utils/result.ts
@@ -1,9 +1,25 @@
-export type Result<T, E> = { ok: T; error?: never } | { err: E; ok?: never };
+export type Ok<T> = {
+  _tag: "success";
+  data: T;
+};
 
-function ok<T>(value: T): Result<T, any> {
-  return { ok: value };
+export type Err<E> = {
+  _tag: "failure";
+  error: E;
+};
+
+export type Result<T, E> = Ok<T> | Err<E>;
+
+export function ok<T>(value: T): Ok<T> {
+  return {
+    _tag: "success",
+    data: value,
+  };
 }
 
-function err<E>(error: E): Result<any, E> {
-  return { err: error };
+export function err<E>(error: E): Err<E> {
+  return {
+    _tag: "failure",
+    error: error,
+  };
 }
